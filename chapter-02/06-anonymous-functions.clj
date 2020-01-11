6d8092b724b3328f705f4d473f70760a69ce4d62
Diego <diego.guisosi@gmail.com>
2020-01-11 12:51:11 -0300

    add name binding examples to anonymous functions

--- chapter-02/06-anonymous-functions.clj
+++ chapter-02/06-anonymous-functions.clj
@@ -5,3 +5,21 @@
 ;function reader macro
 ;#([params*] body), where first param can be %1 or %, second can be %2 and the rest of the params %&
 (filter #(> (count %) 3) ["the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"])
+
+;bind a name to anonymous functions with let
+(require '[clojure.string :as str])
+(defn indexable-words [text]
+  (let [indexable-word? #(> (count %) 3)]
+    (filter indexable-word? (str/split text #"\W+"))))
+
+(indexable-words "The quick brown fox jumps over the lazy dog")
+
+;functions that creates functions
+(defn make-greeting [greeting-prefix]
+  (fn [name] (str greeting-prefix ", " name)))
+
+(def hello-greeting (make-greeting "Hello"))
+(def aloha-greeting (make-greeting "Aloha"))
+
+(hello-greeting "Diego")
+(aloha-greeting "Diego")
